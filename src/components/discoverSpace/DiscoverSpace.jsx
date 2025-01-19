6e4175a4f3e973cab828c7a370cd5383eeb4906d
ShivamTiwari0214 <st472438@gmail.com>
2025-01-19 23:07:29 +0530

    Fixed the aspect ration of the images

--- src/components/discoverSpace/DiscoverSpace.jsx
+++ src/components/discoverSpace/DiscoverSpace.jsx
@@ -4,11 +4,12 @@ import premium from '../ui/image/Premium.jpg'
 import standard from '../ui/image/Standard.jpg'
 import singlePerson from '../ui/icons/singlePerson.svg'
 import people from '../ui/icons/people.svg'
+import gallary from '../ui/image/gallary.jpg'
 const DiscoverSpace = () => {
   return (
-    <div className='mt-10 flex justify-between flex-row flex-wrap gap-4 px-6'>
-      <div>
-        <div className='rounded-lg border overflow-hidden min-h-50 max-w-[150px] bg-white'>
+    <div className='mt-10 flex justify-between flex-row flex-wrap gap-4 px-6 '>
+      <div className='w-[40vw] min-h-50'>
+        <div className='rounded-lg border overflow-hidden   bg-white'>
         <img src={standard} alt="Hotel Room" />
         <div>
 
@@ -26,9 +27,9 @@ const DiscoverSpace = () => {
         </div>
       </div>
       
-      <div>
-        <div className='rounded-lg border overflow-hidden max-h-40 max-w-[150px] min-w-[9.2rem] bg-white'>
-        <img src={premium} alt="Hotel Room" className='h-28 w-full' />
+      <div className='w-[40vw] min-h-50'>
+        <div className='rounded-lg border overflow-hidden   bg-white'>
+        <img src={premium} className='aspect-[4/3]' alt="Hotel Room" />
         <div>
 
           <div className='flex justify-evenly items-center py-2'>
@@ -36,8 +37,8 @@ const DiscoverSpace = () => {
               <img src={people} alt="Person" />
             </div>
             <div>
-              <h4 className='font-poppins text-sm m-0 p-0'>Standard Room</h4>
-              <h6 className='font-poppins text-xs font-light m-0 p-0'>2 People</h6>
+              <h4 className='font-poppins text-sm'>Basic Room</h4>
+              <h6 className='font-poppins text-xs font-light'>Single Person</h6>
             </div>
           </div>
           
@@ -45,8 +46,8 @@ const DiscoverSpace = () => {
         </div>
       </div>
 
-      <div>
-        <div className='rounded-lg border overflow-hidden min-h-50 max-w-[150px] bg-white'>
+      <div className='w-[40vw] min-h-50'>
+        <div className='rounded-lg border overflow-hidden   bg-white'>
         <img src={delux} alt="Hotel Room" />
         <div>
 
@@ -55,8 +56,8 @@ const DiscoverSpace = () => {
               <img src={people} alt="Person" />
             </div>
             <div>
-              <h4 className='font-poppins text-sm'>Premium Room</h4>
-              <h6 className='font-poppins text-xs font-light'>3 People</h6>
+              <h4 className='font-poppins text-sm'>Basic Room</h4>
+              <h6 className='font-poppins text-xs font-light'>Single Person</h6>
             </div>
           </div>
           
@@ -64,14 +65,14 @@ const DiscoverSpace = () => {
         </div>
       </div>
 
-      <div>
-        <div className='rounded-lg border overflow-hidden min-h-50 max-w-[150px] bg-white'>
-        <img src={standard} alt="Hotel Room" />
+      <div className='w-[40vw] min-h-50'>
+        <div className='rounded-lg border overflow-hidden   bg-white'>
+        <img src={gallary} className='aspect-[4/3]' alt="Hotel Room" />
         <div>
 
           <div className='flex justify-evenly items-center py-2'>
             <div>
-              <img src={people} alt="Person" />
+              <img src={singlePerson} alt="Person" />
             </div>
             <div>
               <h4 className='font-poppins text-sm'>Basic Room</h4>
