73b941547fac3c213e820fc2868053df23174b04
ShivamTiwari0214 <st472438@gmail.com>
2025-01-19 19:42:40 +0530

    fixed navbar import

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import Navbar from "./Components/Navbar"
-import HeroSection from "./components/HeroSection"
-import HotelDetails from "./components/hotelDetail/HotelDetails"
-import DiscoverSpace from "./components/DiscoverSpace/DiscoverSpace"
-import Elegance from "./components/elegance/Elegance"
-import Map from "./components/map/Map"
-import Footer from "./components/Footer"
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import HotelDetails from "./components/hotelDetail/HotelDetails";
+import DiscoverSpace from "./components/DiscoverSpace/DiscoverSpace";
+import Elegance from "./components/elegance/Elegance";
+import Map from "./components/map/Map";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
     <div className="h-full w-full bg-[#E2E0DF] pt-5">
       <Navbar />
@@ -17,9 +16,8 @@ function App() {
       <Elegance />
       <Map />
       <Footer />
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
